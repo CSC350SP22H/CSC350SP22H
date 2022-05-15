92fcb19aab66bba0365a7ed4f92d93c7ad6572ea
CSC350SP22H <SAGI.NUGMETULLIN@STU.BMCC.CUNY.EDU>
2022-05-14 22:17:22 -0400

    .

--- proj2.php
+++ proj2.php
@@ -6,19 +6,21 @@ $password= "root";
 $connect=mysqli_connect($servername,$username,$password);
 if(!$connect)  die("Error"); //else echo "connected";
 
-
+session_start();
 $reservation=$_REQUEST['DayReserved'];
 $split=explode(".", $reservation);
 $day=$split[0];
 $slot=intval($split[1]);
 
-$sql = "SELECT apartmentNo FROM laundryshema.people WHERE username = $_SESSION['username']";
-$appN = $result;
+$sql = "SELECT apartmentNo FROM laundryshema.people WHERE username = '".$_SESSION['username']."'";
+$result = mysqli_query($connect, $sql);
+$row = mysqli_fetch_row($result);
+$appN = $row[0];
 
-$sql = "INSERT INTO laundryshema.record(apartmentNo, dayNo, slotNo) VALUES('$appN','$day', '$slot')";
+$sql = "INSERT INTO laundryshema.record(apartmentNo, dayNo, slotNo) VALUES('$appN', '$day', '$slot')";
 $result = mysqli_query($connect, $sql); 	// Send the query to the database
 if($result){
 	echo 'You signed up successfully!';
-	echo $split[2];
+	
 }
 ?>
